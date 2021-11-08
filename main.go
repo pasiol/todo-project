371e6498144fa231f492be31c0fbb8757cfa9f95
Pasi Ollikainen <pasi.ollikainen@pm.me>
2021-11-08 18:02:49 +0200

    1.12

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 type neuteredFileSystem struct {
@@ -32,6 +35,70 @@ func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
 	return f, nil
 }
 
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func getDailyImage() error {
+	filename := "./static/dailyImage.jpg"
+	if fileExists(filename) {
+		fi, err := os.Stat(filename)
+		if err != nil {
+			return err
+		}
+		difference := time.Now().Sub(fi.ModTime())
+		if difference.Hours() < 24 {
+			log.Printf("daily image timestamp is less than 24 hours old")
+			return nil
+		}
+		err = os.Remove(filename)
+		if err != nil {
+			return err
+		}
+	}
+	return getImage(filename)
+}
+
+func getImage(fileName string) error {
+
+	response, err := http.Get("https://picsum.photos/1200")
+	if err != nil {
+		return err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Printf("closing request failed")
+		}
+	}(response.Body)
+	log.Printf("daily image getting statuscode %d", response.StatusCode)
+	if response.StatusCode != 200 {
+		return errors.New("get request failed failed")
+	}
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Printf("closing file failed: %s", err)
+		}
+	}(file)
+
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		return err
+	}
+	log.Printf("updated daily image succesfully")
+	return nil
+}
+
 func main() {
 	r := mux.NewRouter()
 
@@ -40,16 +107,16 @@ func main() {
 		http.ServeFile(w, r, "./web/index.html")
 	})
 
-	port := os.Getenv("PORT")
-	if port == ""  {
-		port = "8000"
-	}
+	port := os.Getenv("APP_PORT")
 
 	server := &http.Server{
-		Addr: fmt.Sprintf(":%s", port),
+		Addr:    fmt.Sprintf(":%s", port),
 		Handler: r,
 	}
-
-	log.Printf("Server started in port %s", port)
+	log.Printf("server started in port %s", port)
+	err := getDailyImage()
+	if err != nil {
+		log.Printf("getting daily image failed: %s", err)
+	}
 	log.Fatal(server.ListenAndServe())
 }
