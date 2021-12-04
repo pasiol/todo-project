ef5f7cd5450b7224698c6211aafb1eb05c17a47a
Pasi Ollikainen <pasi.ollikainen@outlook.com>
2021-12-04 23:59:36 +0200

    4.02

--- main.go
+++ main.go
@@ -8,6 +8,33 @@ import (
 	"os"
 )
 
+func respondWithText(w http.ResponseWriter, code int, payload string) {
+	response := []byte(fmt.Sprintf("<pre>%s</pre>", payload))
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	w.WriteHeader(code)
+	bytes, err := w.Write(response)
+	if err != nil {
+		log.Printf("writing response failed: %s", err)
+	}
+	log.Printf("response bytes %d", bytes)
+}
+
+func getHealth(w http.ResponseWriter, _ *http.Request) {
+	url := fmt.Sprintf("http://%s/health", os.Getenv("APP_PING_PONG_HOST"))
+	response, err := http.Get(url)
+	if err != nil {
+		respondWithText(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	if response.StatusCode == 200 {
+		respondWithText(w, http.StatusOK, "ok")
+		return
+	} else {
+		respondWithText(w, http.StatusInternalServerError, "todo service not responding")
+		return
+	}
+}
+
 func main() {
 
 	r := mux.NewRouter()
@@ -16,6 +43,7 @@ func main() {
 	r.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "/var/app/build/index.html")
 	})
+	r.HandleFunc("/health", getHealth).Methods("GET")
 
 	port := os.Getenv("APP_PORT")
 
--- updateFrontend.sh
+++ updateFrontend.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-rm -rf build && cp -avr ../to-do-project-frontend/build .
+cd ../to-do-project-frontend/
+npm run build
+cd ../todo-project
+rsync -avr ../to-do-project-frontend/build ../todo-project/
