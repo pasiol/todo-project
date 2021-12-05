403072deed505035d9a8eab1a3d44773e4b23c42
Pasi Ollikainen <pasi.ollikainen@outlook.com>
2021-12-05 16:57:55 +0200

    4.05

--- scripts/runLocal.sh
+++ scripts/runLocal.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker stop  todo-project-test
+docker stop todo-project-test
 docker build -t pasiol/todo-project .
 docker run --name todo-project-test --rm -d -p 3000:3000 -e APP_PORT=3000 -e API_URL=172.17.0.1:8888 pasiol/todo-project
